4ac818d022feb10e7818d58e9bf9202d8dd21a47
Hoàng Anh Tuấn <bxh5798@gmail.com>
2019-04-21 11:17:40 +0700

    add slide menu

---
+++ appChat/Controllers/ChatController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  appChat
+//
+//  Created by Hoang Anh Tuan on 4/19/19.
+//  Copyright © 2019 Hoang Anh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ChatController: UIViewController {
+
+    let channelView: UIView = {
+        let vc = UIView()
+        vc.backgroundColor = .white
+        return vc
+    }()
+    
+    let button: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.title = "Smack"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "list")?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleOpenChannelVC))
+    }
+    
+    let blackView = UIView()
+    
+    @objc func handleOpenChannelVC() {
+        if let window = UIApplication.shared.keyWindow {
+
+            blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+            window.addSubview(blackView)
+            window.addSubview(channelView)
+            let width = window.frame.width * 3 / 4
+            channelView.frame = CGRect(x: -width, y: 0, width: width, height: window.frame.height)
+            
+            blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
+            blackView.frame = window.frame
+            blackView.alpha = 0
+
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.blackView.alpha = 1
+                self.channelView.frame = CGRect(x: 0, y: 0, width: width, height: window.frame.height)
+            }, completion: nil)
+        }
+    }
+    
+    @objc func handleDismiss() {
+        print("touching blackView...")
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            if let window = UIApplication.shared.keyWindow {
+                let width = window.frame.width * 3 / 4
+                self.channelView.frame = CGRect(x: -width, y: 0, width: width, height: window.frame.height)
+            }
+        }
+    }
+    
+}
+
